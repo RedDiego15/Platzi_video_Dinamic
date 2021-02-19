e6433c83414db4c2c38540f93e8c41b69b00f354
Diego Rojas <diego-27ro@outlook.com>
2021-02-18 20:47:48 -0600

    changing mobile layout

--- src/js/home.js
+++ src/js/home.js
@@ -4,14 +4,12 @@ const $animation_container = document.querySelector('#animation');
 
 const $primaryPlayList = document.getElementsByClassName('primaryPlaylist');
 
-
-
 const $modal = document.getElementById('modal');
 const $overlay = document.getElementById('overlay');
 const $btn_hideModal = document.getElementById('hide-modal');
 
 const $featuring_container = document.getElementById('featuring');
-const $form = document.getElementById('#form');
+const $form = document.getElementById('form');
 const $modal_title = $modal.querySelector('h1');
 const $modal_image = $modal.querySelector('img');
 const $modal_description = $modal.querySelector('p');
@@ -20,6 +18,7 @@ const $flechas = document.getElementsByClassName('flecha');
 console.log($flechas);
 
 async function load(){
+    createSearchEvent()
     const action_list = await load_movieList_by_genre('action');
     const drama_list = await load_movieList_by_genre('drama');
     const animation_list = await load_movieList_by_genre('animation');
@@ -32,6 +31,14 @@ async function load(){
     giveScroll();
 }
 
+function createSearchEvent(){
+    $form.addEventListener('submit',(event)=>{
+         /*para evitar que la pagina se 
+         recarge en cada momento*/
+         event.preventDefault()
+    })
+}
+
 async function load_movieList_by_genre(genre){
     const response = await fetch('https://yts.mx/api/v2/list_movies.json?genre='+genre)
                             .then(res => res.json())
@@ -39,28 +46,42 @@ async function load_movieList_by_genre(genre){
     return response;   
 }
 function load_movieLists(action_list,drama_list,animation_list){
-    console.log(action_list.data.movies);
-    action_list.data.movies.forEach((movie) => {
-        /*debugger;*/
-        const HTMLString = videoItemTemplate(movie);
-        $action_container.innerHTML += HTMLString;
+    addTemplate(action_list,$action_container);
+    addTemplate(drama_list,$drama_container);
+    addTemplate(animation_list,$animation_container);
+}
+function addTemplate(data_list,$catergory_container){
+    data_list.data.movies.forEach((movie) => {
+        const stringHtml = videoStringItemTemplate(movie);
+        const movieElement = createTemplate(stringHtml);
+        $catergory_container.appendChild(movieElement);
+        
+        eventClickOnMovie(movieElement);
     });
-    drama_list.data.movies.forEach((movie) => {
-        /*debugger;*/
-        const HTMLString = videoItemTemplate(movie);
-        $drama_container.innerHTML += HTMLString;
-    });
-    animation_list.data.movies.forEach((movie) => {
-        /*debugger;*/
-        const HTMLString = videoItemTemplate(movie);
-        $animation_container.innerHTML += HTMLString;
-    });
+}
+function createTemplate(stringHtml){
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML=stringHtml;
+    return html.body.children[0];
+}
+function videoStringItemTemplate(movie){
+    return(
+        `<div class="primaryPlaylistItem">
+            <figure class="primaryPlaylistItem-image">
+                <img src=${movie.medium_cover_image} alt = "image movie">
+            </figure>
+        </div>`
+    );
+}
+function eventClickOnMovie(movieElement){
+    movieElement.addEventListener('click', () => {
+        alert('click');
+    })
 }
 function removeLoadingGif(){
     $action_container.removeChild($action_container.firstElementChild)
     $drama_container.removeChild($drama_container.firstElementChild)
     $animation_container.removeChild($animation_container.firstElementChild)
-
 }
 function appearScrollArrows(){
     for(let btn of $flechas){
@@ -74,9 +95,7 @@ function giveScroll(){
         const $lista_botones = carousel.querySelectorAll('button')
         const $contenedor_carousel = carousel.querySelector('div')
 
-        
         pagination($indicadores,$contenedor_carousel);
-        debugger
         $lista_botones[0].addEventListener('click',function(){action_btn_scroll.bind($lista_botones[0])($contenedor_carousel,$indicadores)})
         $lista_botones[1].addEventListener('click',function(){action_btn_scroll.bind($lista_botones[1])($contenedor_carousel,$indicadores)})   
     }
@@ -100,17 +119,6 @@ function action_btn_scroll(carousel,$indicadores){
         }
     }
 }
-
-function videoItemTemplate(movie){
-    return(
-        `<div class="primaryPlaylistItem">
-            <figure class="primaryPlaylistItem-image">
-                <img src=${movie.medium_cover_image} alt = "image movie">
-            </figure>
-        </div>`
-
-    );
-}
 const numeroPaginas = function numberPages(){
     const $total_peliculas = document.querySelectorAll('#action .primaryPlaylistItem')
     let numberPages;
@@ -119,7 +127,6 @@ const numeroPaginas = function numberPages(){
         return numberPages;
     }else{
         numberPages=Math.ceil($total_peliculas.length/5)
-        debugger
         return numberPages;
     }
 }
@@ -138,14 +145,7 @@ function pagination(container_indicator,carousel){
             e.target.classList.add('activo')
             
 
-        })
-       
+        })  
     }
-    
-    
-
 }
-
-
-
 load();
